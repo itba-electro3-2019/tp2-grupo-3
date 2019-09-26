41f11194188617fdfb5d610709aea0028a082df7
Guido <glambertucci@itba.edu.ar>
2019-09-26 16:49:21 -0300

    Continuacion ejercicio 6

---
+++ Ejercicio 1/dc-sweep/CON CARGA/Risemtl.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+df = pd.read_csv('MOS/1t0.csv')
+plt.title("Rise-Time-RTL-MOS")
+Vin = np.asarray(df['1'])
+Vout = np.asarray(df['3'])
+t= np.asarray(df['x-axis'])
+plt.plot(t,Vin,'r',label = 'Entrada' )
+plt.plot(t,Vout,'g',label = 'Salida' )
+plt.legend()
+plt.ylabel("Vout")
+plt.xlabel("Vin")
+plt.grid()
+print("t-trans  Low to High= ",0.000502232-0.000505063)
+print("t-Rise  Low to High= ",np.abs(0.000501342-0.000502994))
+plt.show()
+df = pd.read_csv('MOS/0t1.csv')
+plt.title("Rise-Time-RTL")
+Vin = np.asarray(df['1'])
+Vout = np.asarray(df['3'])
+t= np.asarray(df['x-axis'])
+plt.plot(t,Vin,'r',label = 'Entrada' )
+plt.plot(t,Vout,'g',label = 'Salida' )
+plt.legend()
+plt.ylabel("Vout")
+plt.xlabel("Vin")
+plt.grid()
+print("t-trans  High to Low= ",1.71032e-7-5.88221e-7)
+print("t-Rise  High to Low= ",-5.23439e-7-1.02291e-7)
+plt.show()
+
+
+
+
+
